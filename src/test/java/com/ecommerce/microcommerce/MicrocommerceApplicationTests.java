f4a80b30fc49420aa415d976ec7713e38497eeff
camilleadam006 <cam.adam@outlook.fr>
2019-03-08 11:54:10 +0100

    Initial commit

---
+++ src/test/java/com/ecommerce/microcommerce/MicrocommerceApplicationTests.java
@@ -0,0 +1,27 @@
+package com.ecommerce.microcommerce;
+
+import com.ecommerce.microcommerce.web.controller.ProductController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicrocommerceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void doSomething(){
+
+		ProductController controller = new ProductController();
+		controller.saySomething();
+		String something = "something";
+		assertEquals(something, controller.saySomething());
+	}
+
+}
